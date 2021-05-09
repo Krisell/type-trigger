3c0c90dedcacddcd3b44e6f6cf4ef19aaf5ac8a4
Martin Krisell <martin.krisell@gmail.com>
2021-05-09 11:27:12 +0200

    Remove unecessary module

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,89 @@
-"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),exports["default"]=void 0;function _classCallCheck(a,b){if(!(a instanceof b))throw new TypeError("Cannot call a class as a function")}function _defineProperties(a,b){for(var c,d=0;d<b.length;d++)c=b[d],c.enumerable=c.enumerable||!1,c.configurable=!0,"value"in c&&(c.writable=!0),Object.defineProperty(a,c.key,c)}function _createClass(a,b,c){return b&&_defineProperties(a.prototype,b),c&&_defineProperties(a,c),a}function _createForOfIteratorHelper(a,b){var c="undefined"!=typeof Symbol&&a[Symbol.iterator]||a["@@iterator"];if(!c){if(Array.isArray(a)||(c=_unsupportedIterableToArray(a))||b&&a&&"number"==typeof a.length){c&&(a=c);var d=0,e=function(){};return{s:e,n:function n(){return d>=a.length?{done:!0}:{done:!1,value:a[d++]}},e:function e(a){throw a},f:e}}throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.")}var f,g=!0,h=!1;return{s:function s(){c=c.call(a)},n:function n(){var a=c.next();return g=a.done,a},e:function e(a){h=!0,f=a},f:function f(){try{g||null==c["return"]||c["return"]()}finally{if(h)throw f}}}}function _unsupportedIterableToArray(a,b){if(a){if("string"==typeof a)return _arrayLikeToArray(a,b);var c=Object.prototype.toString.call(a).slice(8,-1);return"Object"===c&&a.constructor&&(c=a.constructor.name),"Map"===c||"Set"===c?Array.from(a):"Arguments"===c||/^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(c)?_arrayLikeToArray(a,b):void 0}}function _arrayLikeToArray(a,b){(null==b||b>a.length)&&(b=a.length);for(var c=0,d=Array(b);c<b;c++)d[c]=a[c];return d}var isInitialized=!1,entries=[],keypress=function(a){var b,c=_createForOfIteratorHelper(entries);try{for(c.s();!(b=c.n()).done;){var d=b.value;d.word[d.progress]!==a&&(d.progress=0),d.word[d.progress]===a&&(d.progress+=1,d.progress>=d.word.length&&(d.progress=0,"function"==typeof d.cb&&d.cb()))}}catch(a){c.e(a)}finally{c.f()}},TypeTrigger=function(){function a(){_classCallCheck(this,a)}return _createClass(a,null,[{key:"type",value:function type(a){a.split("").forEach(function(a){keypress(a)})}},{key:"register",value:function register(a,b){isInitialized||(window.document.addEventListener("keypress",function(a){keypress(a.key)}),isInitialized=!0),entries.push({word:a,cb:b,progress:0})}}]),a}(),_default=TypeTrigger;exports["default"]=_default;
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _createForOfIteratorHelper(o, allowArrayLike) { var it = typeof Symbol !== "undefined" && o[Symbol.iterator] || o["@@iterator"]; if (!it) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e) { throw _e; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = it.call(o); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e2) { didErr = true; err = _e2; }, f: function f() { try { if (!normalCompletion && it["return"] != null) it["return"](); } finally { if (didErr) throw err; } } }; }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
+var isInitialized = false;
+var entries = [];
+
+var keypress = function keypress(key) {
+  var _iterator = _createForOfIteratorHelper(entries),
+      _step;
+
+  try {
+    for (_iterator.s(); !(_step = _iterator.n()).done;) {
+      var entry = _step.value;
+
+      if (entry.word[entry.progress] !== key) {
+        entry.progress = 0;
+      }
+
+      if (entry.word[entry.progress] === key) {
+        entry.progress += 1;
+
+        if (entry.progress >= entry.word.length) {
+          entry.progress = 0;
+
+          if (typeof entry.cb === 'function') {
+            entry.cb();
+          }
+        }
+      }
+    }
+  } catch (err) {
+    _iterator.e(err);
+  } finally {
+    _iterator.f();
+  }
+};
+
+var TypeTrigger = /*#__PURE__*/function () {
+  function TypeTrigger() {
+    _classCallCheck(this, TypeTrigger);
+  }
+
+  _createClass(TypeTrigger, null, [{
+    key: "type",
+    value: // This is mainly used for testing, but could be useful in application code as well
+    function type(word) {
+      word.split('').forEach(function (character) {
+        keypress(character);
+      });
+    }
+  }, {
+    key: "register",
+    value: function register(word, cb) {
+      if (!isInitialized) {
+        window.document.addEventListener('keypress', function (e) {
+          keypress(e.key);
+        });
+        isInitialized = true;
+      }
+
+      entries.push({
+        word: word,
+        cb: cb,
+        progress: 0
+      });
+    }
+  }]);
+
+  return TypeTrigger;
+}();
+
+var _default = TypeTrigger;
+exports["default"] = _default;
