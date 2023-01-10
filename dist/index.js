dadefab078b4f757d920339f488cbf1125cc9da1
Martin Krisell <martin.krisell@gmail.com>
2023-01-10 12:26:32 +0100

    Update npm dependencies

--- dist/index.js
+++ dist/index.js
@@ -4,61 +4,39 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports["default"] = void 0;
-
+function _typeof(obj) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (obj) { return typeof obj; } : function (obj) { return obj && "function" == typeof Symbol && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }, _typeof(obj); }
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, _toPropertyKey(descriptor.key), descriptor); } }
 function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); Object.defineProperty(Constructor, "prototype", { writable: false }); return Constructor; }
-
-function _createForOfIteratorHelper(o, allowArrayLike) { var it = typeof Symbol !== "undefined" && o[Symbol.iterator] || o["@@iterator"]; if (!it) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e) { throw _e; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = it.call(o); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e2) { didErr = true; err = _e2; }, f: function f() { try { if (!normalCompletion && it["return"] != null) it["return"](); } finally { if (didErr) throw err; } } }; }
-
-function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
-
-function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
-
+function _toPropertyKey(arg) { var key = _toPrimitive(arg, "string"); return _typeof(key) === "symbol" ? key : String(key); }
+function _toPrimitive(input, hint) { if (_typeof(input) !== "object" || input === null) return input; var prim = input[Symbol.toPrimitive]; if (prim !== undefined) { var res = prim.call(input, hint || "default"); if (_typeof(res) !== "object") return res; throw new TypeError("@@toPrimitive must return a primitive value."); } return (hint === "string" ? String : Number)(input); }
 var isInitialized = false;
 var entries = [];
-
 var keypress = function keypress(key) {
-  var _iterator = _createForOfIteratorHelper(entries),
-      _step;
-
-  try {
-    for (_iterator.s(); !(_step = _iterator.n()).done;) {
-      var entry = _step.value;
-
-      if (entry.word[entry.progress] !== key) {
+  for (var _i = 0, _entries = entries; _i < _entries.length; _i++) {
+    var entry = _entries[_i];
+    if (entry.word[entry.progress] !== key) {
+      entry.progress = 0;
+    }
+    if (entry.word[entry.progress] === key) {
+      entry.progress += 1;
+      if (entry.progress >= entry.word.length) {
         entry.progress = 0;
-      }
-
-      if (entry.word[entry.progress] === key) {
-        entry.progress += 1;
-
-        if (entry.progress >= entry.word.length) {
-          entry.progress = 0;
-
-          if (typeof entry.cb === 'function') {
-            entry.cb();
-          }
+        if (typeof entry.cb === 'function') {
+          entry.cb();
         }
       }
     }
-  } catch (err) {
-    _iterator.e(err);
-  } finally {
-    _iterator.f();
   }
 };
-
 var TypeTrigger = /*#__PURE__*/function () {
   function TypeTrigger() {
     _classCallCheck(this, TypeTrigger);
   }
-
   _createClass(TypeTrigger, null, [{
     key: "type",
-    value: // This is mainly used for testing, but could be useful in application code as well
+    value:
+    // This is mainly used for testing, but could be useful in application code as well
     function type(word) {
       word.split('').forEach(function (character) {
         keypress(character);
@@ -73,7 +51,6 @@ var TypeTrigger = /*#__PURE__*/function () {
         });
         isInitialized = true;
       }
-
       entries.push({
         word: word,
         cb: cb,
@@ -81,9 +58,7 @@ var TypeTrigger = /*#__PURE__*/function () {
       });
     }
   }]);
-
   return TypeTrigger;
 }();
-
 var _default = TypeTrigger;
 exports["default"] = _default;
